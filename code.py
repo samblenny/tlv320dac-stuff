053694c22e60993327b56953a5ce44b1b254cf9b
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-08-20 08:25:06 +0000

    get test tone working

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ from micropython import const
 import os
 import synthio
 import sys
-from time import sleep
+import time
 
 from adafruit_tlv320 import TLV320DAC3100
 
@@ -21,13 +21,9 @@ from adafruit_tlv320 import TLV320DAC3100
 SAMPLE_RATE = const(11025)
 CHAN_COUNT  = const(2)
 BUFFER_SIZE = const(1024)
-#==============================================================
-# CAUTION! When this is set to True, the headphone jack will
-# send a line-level output suitable use with a mixer or powered
-# speakers, but that will be _way_ too loud for earbuds. For
-# finer control of volume, you can set dac.dac_volume below.
-LINE_LEVEL  = const(False)
-#==============================================================
+
+DAC_VOLUME       = -10
+HEADPHONE_VOLUME = 0
 
 
 def init_dac_audio_synth(i2c):
@@ -39,34 +35,24 @@ def init_dac_audio_synth(i2c):
     rst = DigitalInOut(PERIPH_RESET)
     rst.direction = Direction.OUTPUT
     rst.value = False
-    sleep(0.1)
+    time.sleep(0.1)
     rst.value = True
-    sleep(0.05)
+    time.sleep(0.05)
     # 2. Configure sample rate, bit depth, and output port
     dac = TLV320DAC3100(i2c)
     dac.configure_clocks(sample_rate=SAMPLE_RATE, bit_depth=16)
     dac.speaker_mute = True
     dac.headphone_output = True
-    # 3. Set volume for for line-level or headphone level
-    if LINE_LEVEL:
-        # This gives a line output level suitable for plugging into a mixer or
-        # the AUX input of a powered speaker (THIS IS TOO LOUD FOR HEADPHONES!)
-        dac.dac_volume = -44
-        dac.headphone_volume = -64
-    else:
-        # This is a reasonable volume for my cheap JVC Gumy earbuds. They tend
-        # to be louder than other headphones, so probably this ought to be a
-        # generally safe volume level. For headphones that need a stronger
-        # signal, carefully increase dac_volume (closer to 0 is louder).
-        dac.dac_volume = -58
-        dac.headphone_volume = -64
+    # 3. Set volume
+    dac.dac_volume = DAC_VOLUME
+    dac.headphone_volume = HEADPHONE_VOLUME
     # 4. Initialize I2S for Fruit Jam rev D
     print("Using default I2S pin definitions (board rev D or newer)")
     audio = I2SOut(bit_clock=I2S_BCLK, word_select=I2S_WS, data=I2S_DIN)
     # 5. Configure synthio patch to generate audio
     vca = synthio.Envelope(
-        attack_time=0.002, decay_time=0.01, sustain_level=0.4,
-        release_time=0, attack_level=0.6
+        attack_time=0, decay_time=0, sustain_level=1.0,
+        release_time=0, attack_level=1.0
     )
     synth = synthio.Synthesizer(
         sample_rate=SAMPLE_RATE, channel_count=CHAN_COUNT, envelope=vca
@@ -84,11 +70,9 @@ def main():
     i2c = I2C()
     (dac, audio, synth) = init_dac_audio_synth(i2c)
 
+    note = 60
+    synth.press(note)
     while True:
-        for note in range(60, 73):
-            synth.press(note)
-            time.sleep(0.8)
-            synth.release(note)
-            time.sleep(0.2)
+        time.sleep(0.5)
 
 main()
